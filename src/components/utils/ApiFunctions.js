28d09fccefa0f1594bc804e2856588c991e34dd8
Ninad More <ninadmore183@gmail.com>
2024-10-08 11:11:19 +0530

    Booking API functions added

--- src/components/utils/ApiFunctions.js
+++ src/components/utils/ApiFunctions.js
@@ -70,3 +70,51 @@ export async function getRoomById(roomId) {
         throw new Error(`Error getting room ${error.message}`);
     }
 } 
+
+// BOOK ROOM
+export async function bookRoom(roomId, booking) {
+    try {
+        const response = await api.get(`/bookings/room/${roomId}/booking`, booking);
+        return response.data;
+    } catch (error) {
+        if(error.response && error.response.data) {
+            throw new Error(`Error: ${error.response.data}`);
+        } else {
+            throw new Error(`Error booking room: ${error.message}`);
+        }
+    }
+}
+
+// GET ALL BOOKINGS
+export async function getAllBookings() {
+    try {
+        const result = await api.get(`/bookings/all-bookings`);
+        return result.data;
+    } catch (error) {
+        throw new Error(`Error fetching room: ${error.message}`);
+    }
+}
+
+// GET BOOKING BY CONFIRMATION CODE
+export async function getBookingByConfirmationCode(confirmationCode) {
+    try {
+        const result = await api.get(`/bookings/confirmation/${confirmationCode}`);
+        return result.data;
+    } catch (error) {
+        if(error.response && error.response.data) {
+            throw new Error(`Error: ${error.response.data}`);
+        } else {
+            throw new Error(`Error finding room: ${error.message}`);
+        }
+    }
+}
+
+// CANCEL BOOKING
+export async function cancelBooking(bookingId) {
+    try {
+        const result = await api.get(`/bookings/booking/${bookingId}/delete`);
+        return result.data;
+    } catch (error) {
+        throw new Error(`Error cancelling booking: ${error.message}`);
+    }
+}
