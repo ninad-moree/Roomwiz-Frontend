b19854f56a0e55cee03aaceb75ae34c50fc27b79
Ninad More <ninadmore183@gmail.com>
2024-10-31 21:33:05 +0530

    Authprovider added to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -15,30 +15,44 @@ import CheckOut from "./components/bookings/CheckOut.jsx";
 import BookingSuccess from "./components/bookings/BookingSuccess.jsx";
 import Bookings from "./components/bookings/Bookings.jsx";
 import FindBooking from "./components/bookings/FindBooking.jsx";
+import Login from "./components/auth/Login.jsx";
+import Registration from "./components/auth/Registration.jsx";
+import Profile from "./components/auth/Profile.jsx";
+import Logout from "./components/auth/Logout.jsx";
+import AuthProvider from "./components/auth/AuthProvider.jsx";
 
 function App() {
   return (
-    <>
+    <AuthProvider>
       <main>
         <Router>
           <NavBar />
           <Routes>
             <Route path="/" element={<Home />}/>
+
             <Route path="/edit-room/:roomId" element={<EditRoom />}/>
             <Route path="/existing-rooms" element={<ExistingRooms />}/>
             <Route path="/add-room" element={<AddRoom />}/>
             <Route path="/browse-all-rooms" element={<RoomListing />}/>
+
             <Route path="/admin" element={<Admin />}/>
+
             <Route path="/book-room/:roomId" element={<CheckOut />}/>
             <Route path="/booking-success" element={<BookingSuccess />}/>
             <Route path="/existing-bookings" element={<Bookings />}/>
             <Route path="/find-booking" element={<FindBooking />}/>
+
+            <Route path="/login" element={<Login />}/>
+            <Route path="/register" element={<Registration />}/>
+            <Route path="/profile" element={<Profile />}/>
+            <Route path="/logout" element={<Logout />}/>
           </Routes>
         </Router>
         <Footer />
       </main>
-    </>
+    </AuthProvider>
   )
 }
 
 export default App
+// 10.27.43
