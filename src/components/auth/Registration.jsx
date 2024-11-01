820ad43e1fab32e7be4d11b49d622e46f1032422
Ninad More <ninadmore183@gmail.com>
2024-11-01 16:25:06 +0530

    Registration UI created

--- src/components/auth/Registration.jsx
+++ src/components/auth/Registration.jsx
@@ -3,35 +3,35 @@ import { registerUser } from '../utils/ApiFunctions';
 import { Link } from 'react-router-dom';
 
 const Registration = () => {
-  const [registration, setRegistration] = useState({firstName: "", lastName: "", email: "", password: ""});
-  const [errorMessage, setErrorMessage] = useState("");
-  const [successMessage, setSuccessMessage] = useState("");
+  	const [registration, setRegistration] = useState({firstName: "", lastName: "", email: "", password: ""});
+	const [errorMessage, setErrorMessage] = useState("");
+	const [successMessage, setSuccessMessage] = useState("");
 
-  const handleInputChange = (e) => {
-    setRegistration({...registration, [e.target.name]: e.target.value});
-  }
+	const handleInputChange = (e) => {
+		setRegistration({...registration, [e.target.name]: e.target.value});
+	}
 
-  const handleRegistration = async () => {
-    e.preventDefault();
+	const handleRegistration = async (e) => {
+		e.preventDefault();
 
-    try {
-      const result = await registerUser(registration);
-      setSuccessMessage(result);
-      setErrorMessage("");
-      setRegistration({firstName: "", lastName: "", email: "", password: ""});
-    } catch (error) {
-      setSuccessMessage("");
-      setErrorMessage(`Registration error: ${error.message}`);
-    }
-    
-    setTimeout(() => {
-      setErrorMessage("");
-      setSuccessMessage("");
-    }, 4000);
-  }
+		try {
+		const result = await registerUser(registration);
+		setSuccessMessage("Resgistered successfully. Please Login to continue");
+		setErrorMessage("");
+		setRegistration({firstName: "", lastName: "", email: "", password: ""});
+		} catch (error) {
+		setSuccessMessage("");
+		setErrorMessage(`Registration error: ${error.message}`);
+		}
+		
+		setTimeout(() => {
+		setErrorMessage("");
+		setSuccessMessage("");
+		}, 4000);
+	}
 
   return (
-    <section className='container col-6 mt-5 mb-5'>
+    <section className='container col-6 mt-5 mb-5 text-start'>
       {errorMessage && <p className='alert alert-danger'>{errorMessage}</p>}
 
       {successMessage && <p className='alert alert-success'>{successMessage}</p>}
@@ -103,8 +103,8 @@ const Registration = () => {
 					<button type="submit" className="btn btn-hotel" style={{ marginRight: "10px" }}>Register</button>
 
 					<span style={{ marginLeft: "10px" }}>
-						Already have an account? 
-            <Link to={"/login"}>Login</Link>
+						Already have an account?  	
+            			<Link to={"/login"}> Login</Link>
 					</span>
 				</div>
 			</form>
